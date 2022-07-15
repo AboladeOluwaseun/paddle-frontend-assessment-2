e36d16400b2f99844bce8ce9d7c5ce46ba002c5d
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-07-15 12:12:05 +0100

    ...

--- src/Components/RepoCard.js
+++ src/Components/RepoCard.js
@@ -11,15 +11,17 @@ const RepoCard = ({
 }) => {
   return (
     <>
-      <div className="mt-4 flex max-w-[80%] mx-auto">
-        <img className="w-[5rem] h-[5rem] rounded-full" src={avatar} alt="" />
-        <div>
-          <p>{reponame}</p>
-          <p>{repodescription}</p>
-          <div className="flex">
-            <p>{stars}</p>
-            <p>{issues}</p>
-            <p>Time Interval</p>
+      <div className="max-w-[100%]">
+        <div className="mt-4 space-x-6 flex   max-w-[50%] mx-auto">
+          <img className="w-[3rem] h-[3rem] rounded-full" src={avatar} alt="" />
+          <div>
+            <p className="font-bold">{reponame}</p>
+            <p className="mt-2 ">{repodescription}</p>
+            <div className="mt-2 flex">
+              <p>{stars}</p>
+              <p>{issues.number}</p>
+              <p>Time Interval</p>
+            </div>
           </div>
         </div>
       </div>
