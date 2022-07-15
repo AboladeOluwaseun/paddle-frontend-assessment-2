568cec916b63b8d5d208b4b03720f2de31c96a44
Maxibrain <aboladeoluwaseungodwithus@gmail.com>
2022-07-15 11:51:12 +0100

    repo displayed

--- src/Components/RepoCard.js
+++ src/Components/RepoCard.js
@@ -1,16 +1,24 @@
 import React from "react";
+import pic from "../mypic.jpg";
 
-const RepoCard = () => {
+const RepoCard = ({
+  avatar,
+  reponame,
+  repodescription,
+  stars,
+  issues,
+  timeinterval,
+}) => {
   return (
     <>
-      <div>
-        <img src="" alt="" />
+      <div className="mt-4 flex max-w-[80%] mx-auto">
+        <img className="w-[5rem] h-[5rem] rounded-full" src={avatar} alt="" />
         <div>
-          <p>Repository Name</p>
-          <p>Repository Description</p>
-          <div>
-            <p>stars</p>
-            <p>issues</p>
+          <p>{reponame}</p>
+          <p>{repodescription}</p>
+          <div className="flex">
+            <p>{stars}</p>
+            <p>{issues}</p>
             <p>Time Interval</p>
           </div>
         </div>
--- src/Pages/GithubPages.js
+++ src/Pages/GithubPages.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import RepoCard from "../Components/RepoCard";
 
 const GithubPages = () => {
+  const [githubrepos, setgithubrepos] = useState([]);
   useEffect(() => {
     fetch(
       "https://api.github.com/search/repositories?q=created:>2021-08-13&sort=stars&order=desc"
@@ -11,11 +12,24 @@ const GithubPages = () => {
         return res.json();
       })
       .then((data) => {
-        console.log(data);
+        // console.log(data);
+        setgithubrepos(data.items);
       });
   }, []);
 
-  return <div>hi</div>;
+  const displayRepos = githubrepos.map((repo) => {
+    return (
+      <RepoCard
+        avatar={repo.owner.avatar_url}
+        reponame={repo.name}
+        repodescription={repo.description}
+        stars={repo.stargazers_count}
+        issues={repo.issues_url}
+        timeinterval={repo.owner.id}
+      ></RepoCard>
+    );
+  });
+  return <div>{displayRepos}</div>;
 };
 
 export default GithubPages;
